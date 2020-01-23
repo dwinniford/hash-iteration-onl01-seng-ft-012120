a1c878580d05598a127514cea5b43d6de958a4d1
Dave Winniford <dwinniford@gmail.com>
2020-01-23 20:04:37 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,8 +7,11 @@
 # }
 
 def select_winner(passengers)
+  winner = ""
   passengers.each do |room, name|
-    
+    if room == :suite_a && name.start_with?("A")
+      winner << name 
+    end 
   end 
-  
+  winner 
 end
